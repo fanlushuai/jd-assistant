b752551c3b3a3760885ee4d289207e1f7fa17c0d
fanlushuai <fanlushuai@github.com>
2020-12-19 03:06:38 +0800

    add cmd参数，--config_file,--sku_id,--buy_time,--aps

--- config.py
+++ config.py
@@ -1,17 +1,34 @@
 # -*- coding: utf-8 -*-
+import argparse
+import configparser
 import os
 
-import configparser
+CMD_SECTION = 'cmd'
 
 
 class Config(object):
 
-    def __init__(self, config_file='config.ini'):
+    def __init__(self, config_file='config.ini', cmd=None):
         self._path = os.path.join(os.getcwd(), config_file)
         if not os.path.exists(self._path):
             raise FileNotFoundError("No such file: config.ini")
         self._config = configparser.ConfigParser()
         self._config.read(self._path, encoding='utf-8')
+
+        # 使用命令行参数，重写部分配置
+        if cmd:
+            if self._config.has_section('sku'):
+                if cmd.sku_id:
+                    self._config.set('sku', 'sku_id', cmd.sku_id)
+                if cmd.buy_time:
+                    self._config.set('sku', 'buy_time', cmd.buy_time)
+
+        # 添加cmd配置section，如果需要的话
+        if not self._config.has_section(CMD_SECTION):
+            self._config.add_section(CMD_SECTION)
+
+        if cmd.aps:
+            self._config.set(CMD_SECTION, 'aps', 'true')
 
     def get(self, section, name, strip_blank=True, strip_quote=True):
         s = self._config.get(section, name)
@@ -26,4 +43,13 @@ class Config(object):
         return self._config.getboolean(section, name)
 
 
-global_config = Config()
+parser = argparse.ArgumentParser(description='jd 抢购')
+parser.add_argument('--config_file', type=str,
+                    help='指定配置文件，可以用于隐藏自己的私有配置.默认配置为config.ini，'
+                         '路径基于当前工作空间目录，比如，--config_file=cookies/config.ini  正好cookies是被gitignone隐藏的。就很ok')
+parser.add_argument('--sku_id', type=str, help='购买商品id.优先级最高')
+parser.add_argument('--buy_time', type=str, help='购买时间.优先级最高')
+parser.add_argument('--aps', action='store_true', help='采用定时调度方式启动')
+cmd_args = parser.parse_args()
+
+global_config = Config(config_file=cmd_args.config_file if cmd_args.config_file else 'config.ini', cmd=cmd_args)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from config import global_config
+import datetime
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from config import global_config, CMD_SECTION
 from jd_assistant import Assistant
 
 """
@@ -51,5 +55,18 @@ def boot_ass():
     # submit_interval: 提交订单失败后重试时间间隔，可选参数，默认5秒
 
 
+def boot():
+    if global_config.getboolean(CMD_SECTION, 'aps'):
+        datetime_obj = datetime.datetime.strptime(buy_time, '%Y-%m-%d %H:%M:%S.%f')
+        # 设定时间的前两分钟再开启 ass。因为里面通过轮训的逻辑来执行定时操作。会消耗cpu。通过定时调度减缓一点消耗。
+        # 先不修改简单用用。看看效果再说。之后考虑重写逻辑全调度器。主要还是考虑精度问题
+        run_date = datetime_obj + datetime.timedelta(minutes=-5)
+        scheduler = BlockingScheduler()
+        scheduler.add_job(boot_ass, 'date', run_date=run_date, id='boot_ass')
+        scheduler.start()
+    else:
+        boot_ass()
+
+
 if __name__ == '__main__':
-    boot_ass()
+    boot()
